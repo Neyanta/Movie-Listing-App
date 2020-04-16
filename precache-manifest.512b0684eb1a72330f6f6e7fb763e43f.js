7b2ce4119c6ae1001d7d8744057da680be508c4b
neyanta <neyantasharma0209@gmail.com>
2020-04-16 06:54:54 +0530

    Updates

---
+++ precache-manifest.512b0684eb1a72330f6f6e7fb763e43f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ad35bb9ae56e731ce1ce38770949e3",
+    "url": "/Movie-Listing-App/index.html"
+  },
+  {
+    "revision": "bb55f91e1aaf04e28b64",
+    "url": "/Movie-Listing-App/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "846ec88eec2a7a529453",
+    "url": "/Movie-Listing-App/static/css/main.5fa3514d.chunk.css"
+  },
+  {
+    "revision": "bb55f91e1aaf04e28b64",
+    "url": "/Movie-Listing-App/static/js/2.2e4bfb67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-Listing-App/static/js/2.2e4bfb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846ec88eec2a7a529453",
+    "url": "/Movie-Listing-App/static/js/main.b0375457.chunk.js"
+  },
+  {
+    "revision": "4592cdc879e33a7a42df",
+    "url": "/Movie-Listing-App/static/js/runtime-main.2f5e8494.js"
+  },
+  {
+    "revision": "1e862516aa31d8cbbb513f111b1ab73c",
+    "url": "/Movie-Listing-App/static/media/preloader.1e862516.gif"
+  }
+]);
